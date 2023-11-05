ffe0a646b99f72c3ef7d05d5c756494337edb48c
Dimitri Kroon <111366411+dimkroon@users.noreply.github.com>
2023-11-06 00:05:44 +0100

    initial tests

---
+++ local/test_resume_playing.py
@@ -0,0 +1,29 @@
+from tests.support import fixtures
+fixtures.global_setup()
+
+import unittest
+
+setUpModule = fixtures.setup_local_tests()
+tearDownModule = fixtures.tear_down_local_tests()
+
+
+from resources.lib import ipwww_resume
+
+from tests.support.testutils import open_json
+
+class Resume(unittest.TestCase):
+    def test_parse_resume(self):
+        data = open_json('html/watching.json')
+        ipwww_resume.parse_watching(data)
+
+
+class PlayStateEnum(unittest.TestCase):
+    def test_instantiate_playstate(self):
+        with self.assertRaises(NotImplementedError):
+            ps = ipwww_resume.PlayState()
+
+    def test_changes_value(self):
+        with self.assertRaises(NotImplementedError):
+            ipwww_resume.PlayState.PLAYING = 1
+            p = getattr(ipwww_resume.PlayState, 'PLAYING')
+            print(p)

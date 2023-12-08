4dc88824b42db202af225a408130a5eaa652eb34
dimkroon <111366411+dimkroon@users.noreply.github.com>
2023-12-08 02:53:02 +0100

    initial tests

---
+++ web/test_watching.py
@@ -0,0 +1,21 @@
+from tests.support import fixtures
+fixtures.global_setup()
+
+from unittest import TestCase
+
+import requests
+
+from resources.lib import ipwww_video, ipwww_common
+from tests.support.testutils import save_json
+
+setUpModule = fixtures.setup_web_test()
+
+
+class Watching(TestCase):
+    def test_get_watching_data(self):
+        page = ipwww_common.OpenURL(url = "https://www.bbc.co.uk/iplayer/watching")
+        data = ipwww_video.ScrapeJSON(page)
+        # save_json(data, 'html/watching.json')
+
+    def test_watching_plain(self):
+        r = ipwww_video.ListWatching()

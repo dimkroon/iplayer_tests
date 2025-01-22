7c9e38b72742d0293652cccfcb053eac03a9542d
dimkroon <111366411+dimkroon@users.noreply.github.com>
2025-01-22 19:56:32 +0100

    [TEST] update tests to current keyboard

--- local/test_ipwww_search.py
+++ local/test_ipwww_search.py
@@ -250,7 +250,7 @@ class TestListSearchItems(TestCase):
     @patch('resources.lib.ipwww_search.SearchHistory._read_file', return_value={'video':{}})
     def test_list_without_saved_items(self, _, p_add_term, p_do_search):
         # Without search term
-        with patch('xbmc.Keyboard', new_callable=keyb_mock, text='new term'):
+        with patch('xbmc.Keyboard', keyb_mock(text='new term')):
             ipwww_search.list_search_terms('video', 130, keyword=None)
             self.assertEqual(0, len(self.li_store))
             p_do_search.assert_called_once_with('new term')
